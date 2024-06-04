efbbceba444dc2930295857acda4b6b29a1c9854
joshuawenata <joshuawenata@gmail.com>
2024-06-04 11:11:26 +0700

    Initial Commit

---
+++ mini2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mini2
+//
+//  Created by Joshua Wenata Sunarto on 04/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mini2/mini2App.swift
@@ -0,0 +1,17 @@
+//
+//  mini2App.swift
+//  mini2
+//
+//  Created by Joshua Wenata Sunarto on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct mini2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
