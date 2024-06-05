84ed597362c637e1ff05694cf104130f95701aa4
joshuawenata <joshuawenata@gmail.com>
2024-06-05 23:24:16 +0700

    feat: create landing page

--- mini2/ContentView.swift
+++ mini2/ContentView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Game Title")
+                .font(.system(size: 50))
+                .padding()
+            VStack(alignment: .leading) {
+                Text("What is your name, adventurer?")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                TextField("Enter your name", text: .constant(""))
+                    .padding()
+                    .background(Color.white)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                    .frame(width: 400)
+            }
+            .padding()
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Text("Start")
+                    .padding()
+                    .frame(width: 200)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
