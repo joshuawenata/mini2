4ae8b369a82a8c412e79c4f03f817c37485228e5
joshuawenata <joshuawenata@gmail.com>
2024-06-09 23:27:14 +0700

    style onboarding

--- mini2/view/OnboardingView.swift
+++ mini2/view/OnboardingView.swift
@@ -11,46 +11,49 @@ import SpriteKit
 struct OnboardingView: View {
     var body: some View {
         NavigationStack {
-            VStack {
+            ZStack {
+                Image("bgLandingPage")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
                 
-                Text("Fall of Aethel")
-                    .font(.custom("JollyLodger", size: 80))
-                    .padding()
-                    .frame(height: 50
-                    )
-                    .padding(.top, 50)
-                    .padding(.bottom, 20)
-                
-                VStack(alignment: .leading) {
+                VStack {
+                    Text("Fall of Aethel")
+                        .font(.custom("JollyLodger", size: 80))
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(height: 50)
+                        .padding(.top, 50)
+                        .padding(.bottom, 20)
                     
-                    Text("What is your name, adventurer?")
-                        .font(.custom("JollyLodger", size: 40))
-                        .frame(height: 50);
-                    
-                    ZStack {
-                        Image("textbox")
-                            .resizable()
-                            .frame(width: 500, height: 80)
-                            .scaledToFit()
+                    VStack(alignment: .leading) {
+                        Text("What is your name, adventurer?")
+                            .font(.custom("JollyLodger", size: 50))
+                            .foregroundColor(.white)
+                            .frame(height: 50)
                         
-                        TextField("Enter your name", text: .constant(""))
-                            .padding()
-                            .frame(width: 400)
+                        ZStack {
+                            Image("textbox")
+                                .resizable()
+                                .frame(width: 500, height: 80)
+                                .scaledToFit()
+                            
+                            TextField("Enter your name", text: .constant(""))
+                                .padding()
+                                .frame(width: 400)
+                        }
                     }
+                    .padding()
                     
+                    Button(action: {
+                        presentGameScene()
+                    }) {
+                        Image("startbutton")
+                            .resizable()
+                            .frame(width: 250, height: 70)
+                    }
                 }
-                .padding()
-                
-                Button(action: {
-                    presentGameScene()
-                }) {
-                    Image("startbutton")
-                        .resizable()
-                        .frame(width: 250, height: 70)
-                }
-                
             }
-            .padding()
         }
     }
     
@@ -59,14 +62,11 @@ struct OnboardingView: View {
         let gameView = SpriteView(scene: scene).ignoresSafeArea()
 
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-          return
+            return
         }
 
         windowScene.windows.first?.rootViewController?.present(
-          UIHostingController(rootView: gameView), animated: true)
-      }
+            UIHostingController(rootView: gameView), animated: true)
+    }
 }
 
-#Preview {
-    OnboardingView()
-}
